22c6e64d8787699027ce9bf657d3e3716ff4981b
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-01 03:15:41 +0600

    basic document maker

--- lib-reportlab/documentmaker.py
+++ lib-reportlab/documentmaker.py
@@ -9,10 +9,10 @@ from reportlab.lib.units import inch
 document = SimpleDocTemplate('budget_template.pdf',
                             pagesize=letter,
                             rightMargin=100, leftMargin=100,
-                            topMargin=100, bottomMargin=100)
+                            topMargin=0, bottomMargin=100)
 story = []
 logo = 'DPI-logo.png'
-title = 'DPI Budget Approval Form'
+title = '<font size="25">DPI Budget Approval Form</font>'
 proposer = 'Tareq Monwer'
 proposer_design = 'Web Developer'
 proposer_department = 'Administration'
@@ -26,8 +26,14 @@ styles.add(ParagraphStyle(name='Centered', alignment=TA_CENTER))
 story.append(Paragraph(title, styles['Centered']))
 story.append(Spacer(1, 50))
 
+story.append(Paragraph('To', styles['Normal']))
+story.append(Paragraph('The Chief Executive Officer', styles['Normal']))
+story.append(Spacer(1, 30))
 
-
+story.append(Paragraph(f'PROPOSED BY: {proposer}', styles['Normal']))
+# story.append(Spacer(1, 30))
+story.append(Paragraph(f'DEPARTMENT: {proposer_department}', styles['Normal']))
+# story.append(Spacer(1, 30))
 
 # Finally, make the file based of story list.
 document.build(story)
