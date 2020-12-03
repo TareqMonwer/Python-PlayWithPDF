84555f81d01d99aaca53fbb4275e3408ef7a0cc4
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-03 21:44:37 +0600

    qr_code generator

---
+++ lib-reportlab/qr_code.py
@@ -0,0 +1,24 @@
+from reportlab.graphics import renderPDF
+from reportlab.graphics.barcode import qr
+from reportlab.graphics.shapes import Drawing
+from reportlab.pdfgen import canvas
+
+
+def qr_code(barcode_value):
+    c = canvas.Canvas('qr_code.pdf')
+    qrcode = qr.QrCodeWidget(barcode_value)
+    qrcode.barWidth = 145
+    qrcode.barHeight = 145
+    qrcode.qrVersion = 1
+    # bounds = qrcode.getBounds()
+    # width = bounds[2] - bounds[1]
+    # height = bounds[3] - bounds[1]
+    d = Drawing()
+    d.add(qrcode)
+    renderPDF.draw(d, c, 15, 700)
+    c.save()
+
+
+
+if __name__ == "__main__":
+    qr_code('www.tareqmonwer.com')
