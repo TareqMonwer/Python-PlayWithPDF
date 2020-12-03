f21d7a811f080f108714fd28ceb87e8b1d355ca2
tareqmonwer <tareqmonwer137@gmail.com>
2020-12-03 22:35:57 +0600

    qr_code generator using qrcode package with reportlib

---
+++ lib-reportlab/qr_code_pack.py
@@ -0,0 +1,20 @@
+import qrcode
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Image
+
+
+def get_qr_code(barcode_value):
+    doc = SimpleDocTemplate('qr_code2.pdf')
+    flowables = []
+
+    qr_img = qrcode.make(barcode_value)
+    qr_img.save('test_qr.png')
+    flowables.append(Image('test_qr.png'))
+
+    doc.build(flowables)
+
+
+
+if __name__ == "__main__":
+    get_qr_code('www.tareqmonwer.com')
